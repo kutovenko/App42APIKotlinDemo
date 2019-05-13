27f84a4d2958e03e3803de5f2feceded7465c049
kutovenko <kutovenko@gmail.com>
2019-05-13 12:33:30 +0300

    Initial commit

--- app/src/main/java/com/blogspot/alexeykutovenko/app42apikotlindemo/ui/MainActivity.kt
+++ app/src/main/java/com/blogspot/alexeykutovenko/app42apikotlindemo/ui/MainActivity.kt
@@ -23,7 +23,6 @@ class MainActivity : AppCompatActivity(), App42HelperService.App42StorageService
         json.put("age",30)
     }
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
